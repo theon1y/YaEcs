b14186367000bb8a24881790ec322bb981679c0a
Maksim Skrupski <skrupski@reaction.games>
2022-05-18 10:17:55 +0500

    Add first update step

--- YaECS/Bootstrap/UpdateSteps.cs
+++ YaECS/Bootstrap/UpdateSteps.cs
@@ -2,8 +2,9 @@
 {
     public static class UpdateSteps
     {
-        public static UpdateStep EarlyUpdate = new UpdateStep(nameof(EarlyUpdate), 1024);
-        public static UpdateStep Update = new UpdateStep(nameof(Update), 2048);
-        public static UpdateStep LateUpdate = new UpdateStep(nameof(LateUpdate), 4096);
+        public static UpdateStep First = new(nameof(First), 1);
+        public static UpdateStep EarlyUpdate = new(nameof(EarlyUpdate), 1024);
+        public static UpdateStep Update = new(nameof(Update), 2048);
+        public static UpdateStep LateUpdate = new(nameof(LateUpdate), 4096);
     }
 }
